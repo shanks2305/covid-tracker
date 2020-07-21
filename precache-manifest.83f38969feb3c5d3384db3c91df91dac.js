abd8ec4575b3baada01a953d3ec46df2aee2c122
shanks2305 <contactshishirbahuguna@gmail.com>
2020-07-21 15:34:25 +0530

    Updates

---
+++ precache-manifest.83f38969feb3c5d3384db3c91df91dac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8980093e71e7d2d3f7dc15f0f1e49f10",
+    "url": "/covid-tracker/index.html"
+  },
+  {
+    "revision": "80b32c9ece97dd3b0ff5",
+    "url": "/covid-tracker/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "f66fee62ab6fda0ad106",
+    "url": "/covid-tracker/static/css/main.cd83766d.chunk.css"
+  },
+  {
+    "revision": "80b32c9ece97dd3b0ff5",
+    "url": "/covid-tracker/static/js/2.06f446df.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid-tracker/static/js/2.06f446df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f66fee62ab6fda0ad106",
+    "url": "/covid-tracker/static/js/main.87952ace.chunk.js"
+  },
+  {
+    "revision": "a2c09be945803fa375fb",
+    "url": "/covid-tracker/static/js/runtime-main.948b6ba1.js"
+  }
+]);
